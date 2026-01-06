7eec22ca74d111a96d1ae32a21993e614ff286dc
wt8888zz-droid <wt8888zz@gmail.com>
2026-01-06 17:47:34 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = "wallet-demo-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(()=>caches.match("./index.html")))
+  );
+});
